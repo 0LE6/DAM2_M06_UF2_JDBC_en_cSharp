5ce90f95da950d25c2bb33df8411d9e3249fd75a
Oleg <kharenkoleg@gmail.com>
2023-11-06 00:07:38 +0100

    Agregar archivos de proyecto.

---
+++ DAM2_M06_UF2_JDBC_en_cSharp/Doctor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M06_UF2_JDBC_en_cSharp
+{
+    public class Doctor
+    {
+        // Propiedades que encapsulan los atributos del Doctor
+        public int DoctorCode { get; set; }
+        public string Name { get; set; }
+        public int HospitalCode { get; set; }
+        public string Specialization { get; set; }
+
+        // Constructor sin parámetros
+        public Doctor() { }
+
+        // Constructor con parámetros
+        public Doctor(int doctorCode, string name, int hospitalCode, string specialization)
+        {
+            DoctorCode = doctorCode;
+            Name = name;
+            HospitalCode = hospitalCode;
+            Specialization = specialization;
+        }
+    }
+}
---
+++ DAM2_M06_UF2_JDBC_en_cSharp/Program.cs
@@ -0,0 +1,81 @@
+﻿using DAM2_M06_UF2_JDBC_en_cSharp;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Aquí creamos nuestro nuevo Doctor.
+        Doctor drCSharp = new Doctor(999, "CSharp", 18, "SQL from C#");
+        int newHospitalCode = 22;
+
+        if (CreateAndUpdate(drCSharp, newHospitalCode))
+            Console.WriteLine("SUCCESFULLY CREATED & UPDATED");
+        else Console.WriteLine("FAIL TO CREATED & UPDATED");
+
+        Console.WriteLine("\n"); // Separamos
+
+        Doctor drFail = new Doctor();
+        drFail.DoctorCode = 70;
+        drFail.Name = "Fail";
+        drFail.HospitalCode = 18;
+        drFail.Specialization = "Get Exceptions";
+
+        // Con un código de hospital inexistente
+        if (CreateAndUpdate(drFail, 666))
+            Console.WriteLine("SUCCESFULLY CREATED & UPDATED");
+        else Console.WriteLine("FAIL TO CREATED & UPDATED");
+    }
+
+    public static bool CreateAndUpdate(Doctor d, int updateHospitalCode)
+    {
+        string user = "root"; string pass = "";
+        bool everythingIsOk = true;
+        string connectionString = $"Server=localhost;Database=hospital;User={user};Password={pass};";
+
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            // Esto es el quivalente de Java --> Connection con = DriverManager.getConnection...
+            connection.Open();
+            MySqlTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                // 1st - Llamamos al procedimiento almacenado para crear un nuevo doctor
+                using (MySqlCommand command = new MySqlCommand("CreateDoctor", connection, transaction))
+                {
+                    // Importante, especificamos que a lo que vamos a entrar es a una Procedure
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.Add(new MySqlParameter("@doctorCode", d.DoctorCode));
+                    command.Parameters.Add(new MySqlParameter("@doctorName", d.Name));
+                    command.Parameters.Add(new MySqlParameter("@hospitalCode", d.HospitalCode));
+                    command.Parameters.Add(new MySqlParameter("@docSpec", d.Specialization));
+                    command.ExecuteNonQuery();
+                }
+
+                // 2nd - Llamamos al procedimiento almacenado para actualizar el doctor
+                using (MySqlCommand command = new MySqlCommand("UpdateDoctorHospital", connection, transaction))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add(new MySqlParameter("@DoctorCode", d.DoctorCode));
+                    command.Parameters.Add(new MySqlParameter("@newHospitalCode", updateHospitalCode));
+                    command.ExecuteNonQuery();
+                }
+
+                transaction.Commit();
+                Console.WriteLine("Transaction committed successfully!");
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                Console.WriteLine("Transaction rolled back due to an Exception");
+                Console.WriteLine(e.Message);
+                everythingIsOk = false;
+            }
+        }
+
+        return everythingIsOk;
+    }
+}
